73404acef3849ad1585c08db2fd3ef0fd28fec15
WIN-KUYOU\DESKTOP <2357622935@qq.com>
2023-04-19 17:46:02 +0800

    initial commit

---
+++ src/main/java/org/ryuu/learn/spring/dynamicdatasource/LearnSpringDynamicDataSourceApplication.java
@@ -0,0 +1,11 @@
+package org.ryuu.learn.spring.dynamicdatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringDynamicDataSourceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpringDynamicDataSourceApplication.class, args);
+    }
+}
